905fe3f2026710490a2a65d06cb838a6d323de54
Ignacio Gramajo <igramajo@outlook.com>
2022-10-13 01:29:14 -0700

    Initial Commit

---
+++ VALtracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VALtracker
+//
+//  Created by Ignacio Gramajo on 10/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
