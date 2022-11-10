09cdd1c8c13fcdc0f3f830989f8dae7ad90b4ae7
Ignacio Gramajo <igramajo@outlook.com>
2022-11-10 00:46:55 -0800

    Initial work on Landing Page
    Added welcome message to verify logged in user. field to search by ID

---
+++ VALtracker/LandingViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LandingViewController.swift
+//  VALtracker
+//
+//  Created by Ignacio Gramajo on 11/10/22.
+//
+
+import UIKit
+import Parse
+
+class LandingViewController: UIViewController {
+
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var IDField: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let currentUser = PFUser.current()
+        welcomeLabel.text = "Welcome, " + (currentUser?.username ?? "NULL") + "!"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func searchById(_ sender: Any) {
+        let riotID = IDField.text
+        
+        //Matt your stuff will go here
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
