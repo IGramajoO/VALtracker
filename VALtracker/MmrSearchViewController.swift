e6451e54e01754d3050ed1f362107b41934941a1
MattPeters13 <mapeters@csumb.edu>
2022-12-09 13:48:20 -0800

    added code to pull from when we add a table view to search page

--- VALtracker/MmrSearchViewController.swift
+++ VALtracker/MmrSearchViewController.swift
@@ -22,6 +22,15 @@ class MmrSearchViewController: UIViewController {
     //    var result = id.split(separator: ch)
     //    print("Result : \(result)")
     
+    //For recent matches
+    var myTeamScores: [Int] = []
+    var enemyTeamScores: [Int] = []
+    var agentsPlayed: [String] = []
+    var mySide: [Int] = []
+    var redTeamPlayers = [[String]]()
+    var blueTeamPlayers = [[String]]()
+    var mapsPlayed: [String] = []
+    
     @IBOutlet weak var nameLabel: UILabel!
     
     @IBOutlet weak var rankLabel: UILabel!
@@ -153,5 +162,124 @@ class MmrSearchViewController: UIViewController {
              */
             
         }
+    
+    func reqMatches(){
+//        viewMatchLabel.isEnabled = false
+
+        //NAME AND TAGLINE GO HERE LIKE THIS na/\(nameID)/\(tagline)
+        // nameID and tagline come from the variables after the character split in ViewDidLoad before the req() call
+        let url = URL(string: "https://api.henrikdev.xyz/valorant/v3/matches/na/\(nameID)/\(tagline)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDict = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                //                print(dataDict)
+                let arr = dataDict["data"] as! [[String: Any]]
+           
+                // print(arrA)
+                //  print("*************")
+                // print(arrB)
+//                print("***********")
+                //either red or blue team
+                var myTeamsArray = []
+                var enemyTeamsArray = []
+                
+                //scored depedning on if the users team is red or blue. The opposite is put into enemyTeamScores
+
+                var myTeam = "none"
+                var name = "noName"
+                
+                for k in 0...4{
+                    let metadata = arr[k]["metadata"] as! [String: Any]
+                    let players = arr[k]["players"] as! [String: Any]
+                    let indv = players["all_players"] as! Array<Any>
+                    let testPlayers = []
+                    var redTeamP = [String]()
+                    var blueTeamP = [String]()
+                    
+                    self.mapsPlayed.append(metadata["map"] as! String)
+                    
+                    var me = indv[0] as! [String: Any]
+                    var keyIndexPlayer = 0
+                    
+                    for i in 0...9{
+                        me = indv[i] as! [String: Any]
+//                        print("==================")
+//                        print(me["stats"]) //me["stats"] for KDA
+//                        print("==================")
+                        name = me["name"] as! String
+                        //NAME CHANGE HERE
+                        if(me["team"] as! String == "Red"){
+                            redTeamP.append(name)
+                        }
+                        else{
+                            blueTeamP.append(name)
+                        }
+                        
+                        if(name == self.nameID){
+                            keyIndexPlayer = i
+                            myTeam = me["team"] as! String
+                            myTeamsArray.append(myTeam)
+                            self.agentsPlayed.append(me["character"] as! String)
+//                            break
+                        }
+                    }
+                    self.redTeamPlayers.append(redTeamP)
+                    self.blueTeamPlayers.append(blueTeamP)
+
+                }
+                print("==================")
+
+//                print(self.redTeamPlayers)
+//                print(self.blueTeamPlayers)
+//                print(arr[i])
+                print("==================")
+
+
+                //print(name)
+                //print(myTeam)
+                //print(myTeamsArray)
+                //print(enemyTeamsArray)
+                
+                var redTeam = []
+                var blueTeam = []
+                for i in 0...4{
+                    let teams = arr[i]["teams"] as! [String: Any]
+                    let red = teams["red"] as! [String: Any]
+                    let blue = teams["blue"] as! [String: Any]
+                    redTeam.append(red["rounds_won"] as! Int)
+                    blueTeam.append(blue["rounds_won"] as! Int)
+                }
+//                print(redTeam)
+//                print(blueTeam)
+                
+                for i in 0...4{
+                    if(myTeamsArray[i] as! String == "Red"){
+                        self.mySide.append(1)
+                        self.myTeamScores.append(redTeam[i] as! Int)
+                        self.enemyTeamScores.append(blueTeam[i] as! Int)
+                    }
+                    else{
+                        self.mySide.append(0)
+                        self.myTeamScores.append(blueTeam[i] as! Int)
+                        self.enemyTeamScores.append(redTeam[i] as! Int)
+                    }
+                }
+//                print("##############")
+//                print(self.myTeamScores)
+//                print(self.enemyTeamScores)
+//                print("^^^^^^^^^^^^^^^^^^^")
+//                print(self.agentsPlayed)
+                
+            }
+        }
+        
+//        viewMatchLabel.alpha = 1.0
+        task.resume()
+    }
     }
 
